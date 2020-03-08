e661b1a549c8d669795d33b87d061383cd17f85e
Alisson Youssf <youssfbr@gmail.com>
2020-03-08 20:11:56 -0300

    Implementando a view de cadastro

--- src/main/java/br/com/alissondev/springwebmvc/controller/JediController.java
+++ src/main/java/br/com/alissondev/springwebmvc/controller/JediController.java
@@ -21,4 +21,14 @@ public class JediController {
 
         return modelAndView;
     }
+
+    @GetMapping("/new-jedi")
+    public ModelAndView newJedi() {
+
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new-jedi");
+
+        modelAndView.addObject("jedi", new Jedi());
+        return modelAndView;
+    }
 }
