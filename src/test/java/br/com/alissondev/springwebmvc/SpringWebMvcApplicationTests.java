17aea396c4ed9402d7e09968e0d1bdf938bc009f
Alisson Youssf <youssfbr@gmail.com>
2020-03-08 16:19:17 -0300

    Projeto criado e templates html

---
+++ src/test/java/br/com/alissondev/springwebmvc/SpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alissondev.springwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
