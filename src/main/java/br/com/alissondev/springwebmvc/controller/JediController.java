12a07bf69d5bef22969f851b5d03aade0b9682e6
Alisson Youssf <youssfbr@gmail.com>
2020-03-09 14:28:55 -0300

    Salvando no banco

--- src/main/java/br/com/alissondev/springwebmvc/controller/JediController.java
+++ src/main/java/br/com/alissondev/springwebmvc/controller/JediController.java
@@ -5,36 +5,49 @@ import br.com.alissondev.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.naming.Binding;
+
 import javax.validation.Valid;
-import java.util.List;
+
+import java.util.Optional;
 
 @Controller
 public class JediController {
 
     @Autowired
-    private JediRepository jediRepository;
+    private JediRepository repository;
 
     @GetMapping("/jedi")
-    public ModelAndView jedi() {
+    public ModelAndView jedi(){
 
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("jedi");
 
-        modelAndView.addObject("allJedi", jediRepository.getAllJedi());
+
+        modelAndView.addObject("allJedi", repository.findAll());
 
         return modelAndView;
     }
 
+    @GetMapping("/search")
+    public ModelAndView search(@RequestParam(value = "name") final String name) {
+
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+
+        modelAndView.addObject("allJedi", repository.findByNameContainingIgnoreCase(name));
+
+        return modelAndView;
+    }
+
+
     @GetMapping("/new-jedi")
-    public ModelAndView newJedi() {
+    public ModelAndView newJedi(){
 
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("new-jedi");
@@ -44,16 +57,40 @@ public class JediController {
     }
 
     @PostMapping("/jedi")
-    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirectAttributes) {
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirectAttributes){
 
-        if (result.hasErrors()) {
+        if(result.hasErrors()){
             return "new-jedi";
         }
 
-        jediRepository.add(jedi);
+        repository.save(jedi);
 
-        redirectAttributes.addFlashAttribute("message", "Jedi cadastrado com sucesso.");
+        redirectAttributes.addFlashAttribute("message", "Jedi cadatrado com sucesso.");
 
-        return  "redirect:jedi";
+        return "redirect:jedi";
+
     }
+
+    @GetMapping("/jedi/{id}/delete")
+    public String deleteJedi(@PathVariable("id") final Long id, RedirectAttributes redirectAttributes) {
+
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        repository.delete(jedi.get());
+
+        redirectAttributes.addFlashAttribute("message", "Jedi removido com sucesso.");
+
+        return "redirect:/jedi" ;
+    }
+
+    @GetMapping("/jedi/{id}/update")
+    public String updateJedi(@PathVariable("id") final Long id, Model model) {
+
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        model.addAttribute("jedi", jedi.get());
+
+        return "edit-jedi";
+    }
+
 }
--- src/main/java/br/com/alissondev/springwebmvc/repository/JediRepository.java
+++ src/main/java/br/com/alissondev/springwebmvc/repository/JediRepository.java
@@ -1,30 +1,14 @@
 package br.com.alissondev.springwebmvc.repository;
 
 import br.com.alissondev.springwebmvc.model.Jedi;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class JediRepository {
-    private List<Jedi> jedi;
+public interface JediRepository extends JpaRepository<Jedi, Long> {
 
-    public JediRepository() {
-        jedi = new ArrayList<>();
-        jedi.add(new Jedi("Luke", "Skywalker"));
-    }
+    List<Jedi> findByNameContainingIgnoreCase(final String name);
 
-    public List<Jedi> getAllJedi() {
-
-        return this.jedi;
-    }
-
-    public void add(final Jedi jedi) {
-        this.jedi.add(jedi);
-    }
-
-    public List<Jedi> findAll() {
-        return this.jedi;
-    }
 }
