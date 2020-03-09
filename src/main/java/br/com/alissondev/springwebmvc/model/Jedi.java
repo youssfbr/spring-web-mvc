d8441b1003a7c9cf9fbd924d141643592da3af22
Alisson Youssf <youssfbr@gmail.com>
2020-03-09 14:15:49 -0300

    Adicionando dependências (JPA, H2) e configurando Entidade

--- src/main/java/br/com/alissondev/springwebmvc/model/Jedi.java
+++ src/main/java/br/com/alissondev/springwebmvc/model/Jedi.java
@@ -1,21 +1,33 @@
 package br.com.alissondev.springwebmvc.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "jedi")
 public class Jedi {
 
-    @NotBlank
+    @Id
+    @Column(name="id_jedi")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Size(min = 3, max = 10, message = "Nome deve conter entre 3 e 10 caracteres")
+    @NotBlank(message = "Nome não pode estar em branco")
+    @Column(name = "name")
     private String name;
 
-    @NotBlank
+    @NotBlank(message = "Sobrenome não pode estar em branco")
+    @Column(name = "last_name")
     private String lastName;
 
-    public Jedi(final String name, final String lastname) {
+    public Jedi (final String name, final String lastname) {
         this.name = name;
         this.lastName = lastname;
     }
 
-    public Jedi() {
+    public Jedi () {
     }
 
     public String getName() {
