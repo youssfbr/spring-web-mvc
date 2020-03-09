f8bb1f227c9f4a0367aa1eface58cfb2a8d8f27b
Alisson Youssf <youssfbr@gmail.com>
2020-03-09 11:24:47 -0300

    Injetando dependência

--- src/main/java/br/com/alissondev/springwebmvc/rest/JediResource.java
+++ src/main/java/br/com/alissondev/springwebmvc/rest/JediResource.java
@@ -1,7 +1,12 @@
 package br.com.alissondev.springwebmvc.rest;
 
+import br.com.alissondev.springwebmvc.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JediResource {
+
+    @Autowired
+    private JediRepository repository;
 }
